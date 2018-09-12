60b5e2e2eb1f84f008874ad499eaa371e7d8446d
Zunchao <zunchao.zheng@gmail.com>
2018-09-12 21:13:00 +0200

    change folder

--- gym_pre_arms/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='arm3joints-v0',
-    entry_point='gym_pre_arms.envs:Arm3jointsEnv',
-)
--- gym_pre_arms/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_pre_arms.envs.arm3joints_env import Arm3jointsEnv
