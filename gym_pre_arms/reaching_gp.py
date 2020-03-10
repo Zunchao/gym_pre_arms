5624e073148c2b26075768091b7edfdb6ff072a1
Zunchao Zheng <zunchao.zheng@student.kit.edu>
2020-03-10 12:00:19 +0100

    update reaching gp

--- gym_pre_arms/reaching_gp.py
+++ gym_pre_arms/reaching_gp.py
@@ -2,6 +2,12 @@ import GPy
 from IPython.display import display
 import numpy as np
 from csv_writer_reader import CSV_Writer_Reader
+import os
+import inspect
+import pickle
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+os.sys.path.insert(0, parentdir)
 
 class ReachingGPy():
     def __init__(self):
@@ -12,9 +18,12 @@ class ReachingGPy():
         m = GPy.models.GPRegression(X, Y, kernel)
         m.optimize(messages=True)
         display(m)
-        np.save('model_save.npy', m.param_array)
+        np.save('model_save11.npy', m.param_array)
 
-    def gp_load(self, X, Y):
+        with open('/home/zheng/ws_xiao/gymtestresults/model_save2.dump', 'wb') as buff:
+            pickle.dump(m, buff)
+
+    def gp_load(self, X, Y, x0):
         m_load = GPy.models.GPRegression(X, Y, initialize=False)
         m_load.update_model(False)  # do not call the underlying expensive algebra on load
         m_load.initialize_parameter()  # Initialize the parameters (connect the parameters up)
@@ -22,17 +31,26 @@ class ReachingGPy():
         m_load.update_model(True)  # Call the algebra only once
         #print(m_load)
         display(m_load)
+        y, ysigma = m_load.predict(Xnew=np.array([x0]))
+        print('final 1 : ', y, ysigma)
+
+        model = pickle.load(open('/home/zheng/ws_xiao/gymtestresults/model_save2.dump', "rb"))
+        y, ysigma = model.predict(Xnew=np.array([x0]))
+        print('final 2 : ', y, ysigma)
 
 
 if __name__ == '__main__':
-    pathin = '/home/zheng/ws_xiao/gymtestresults/inputX_ori_rand.csv'
-    pathout = '/home/zheng/ws_xiao/gymtestresults/outputY_ori_rand.csv'
+    pathin = currentdir+'/gym_pre_arms/envs/inputX_ori_rand.csv'
+    pathout = currentdir+'/gym_pre_arms/envs/outputY_ori_rand.csv'
+    print(pathin)
     # split into input (X) and output (Y) variables
     csvrw = CSV_Writer_Reader()
     X = csvrw.readcsv(filepath=pathin)
     Y = csvrw.readcsv(filepath=pathout)
-    x = np.array(X)
-    y = np.array(Y)
+    x = np.array(X[0:90000])
+    y = np.array(Y[0:90000])
+    x0 = np.array(X[20002])
 
     test = ReachingGPy()
-    test.gp_process(x,y)
+    test.gp_process(x, y)
+    #test.gp_load(x,y, x0)
