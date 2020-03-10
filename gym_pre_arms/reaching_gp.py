fd91028f7eac314500140e39885003c62c87b6ca
Zunchao Zheng <zunchao.zheng@student.kit.edu>
2020-03-10 12:03:12 +0100

    update reaching gp

--- gym_pre_arms/reaching_gp.py
+++ gym_pre_arms/reaching_gp.py
@@ -14,13 +14,13 @@ class ReachingGPy():
         self.path = ''
 
     def gp_process(self, X, Y):
-        kernel = GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
+        kernel = GPy.kern.RBF(input_dim=4, variance=1., lengthscale=1.)
         m = GPy.models.GPRegression(X, Y, kernel)
         m.optimize(messages=True)
         display(m)
         np.save('model_save11.npy', m.param_array)
 
-        with open('/home/zheng/ws_xiao/gymtestresults/model_save2.dump', 'wb') as buff:
+        with open(currentdir+'/model_save12.dump', 'wb') as buff:
             pickle.dump(m, buff)
 
     def gp_load(self, X, Y, x0):
@@ -34,23 +34,24 @@ class ReachingGPy():
         y, ysigma = m_load.predict(Xnew=np.array([x0]))
         print('final 1 : ', y, ysigma)
 
-        model = pickle.load(open('/home/zheng/ws_xiao/gymtestresults/model_save2.dump', "rb"))
+        model = pickle.load(open(currentdir+'/model_save12.dump', "rb"))
         y, ysigma = model.predict(Xnew=np.array([x0]))
         print('final 2 : ', y, ysigma)
 
 
 if __name__ == '__main__':
-    pathin = currentdir+'/gym_pre_arms/envs/inputX_ori_rand.csv'
-    pathout = currentdir+'/gym_pre_arms/envs/outputY_ori_rand.csv'
+    pathin = currentdir+'/inputX_ori_rand.csv'
+    pathout = currentdir+'/outputY_ori_rand.csv'
     print(pathin)
     # split into input (X) and output (Y) variables
     csvrw = CSV_Writer_Reader()
     X = csvrw.readcsv(filepath=pathin)
     Y = csvrw.readcsv(filepath=pathout)
-    x = np.array(X[0:90000])
-    y = np.array(Y[0:90000])
-    x0 = np.array(X[20002])
+
+    x = np.array(X[0:19999])
+    y = np.array(Y[0:19999])
+    x0 = np.array(X[99999])
 
     test = ReachingGPy()
     test.gp_process(x, y)
-    #test.gp_load(x,y, x0)
+    test.gp_load(x,y, x0)
